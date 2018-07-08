25db0cb6829eb7673ff95f365243dd6c089a236a
Fred Honorio <2962247+fredshonorio@users.noreply.github.com>
2018-07-08 13:09:26 +0100

    Typeclasses, monoids, functors

--- src/main/scala/learnfp/typeclass/Show.scala
+++ src/main/scala/learnfp/typeclass/Show.scala
@@ -12,14 +12,14 @@ object Printer {
 
 object ShowInstances {
   implicit val intInstance:Show[Int] = new Show[Int] {
-    override def show(x: Int): String = ???
+    override def show(x: Int): String = java.lang.Integer.toString(x)
   }
 
   implicit val doubleInstance:Show[Double] = new Show[Double] {
-    override def show(x: Double): String = ???
+    override def show(x: Double): String = java.lang.Double.toString(x)
   }
 
   implicit def listInstance[T](implicit xShow:Show[T]):Show[List[T]] = new Show[List[T]] {
-    override def show(xs:List[T]): String = ???
+    override def show(xs:List[T]): String = xs.map(xShow.show).mkString("[", ", ", "]")
   }
 }
